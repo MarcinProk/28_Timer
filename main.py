57114594ebda4b083d50164aa4748c9a840fceff
Marcin Prok <132586544+MarcinProk@users.noreply.github.com>
2024-01-07 11:03:06 +0100

    Update main.py
    update file path of pic

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ window.title('Timer')
 window.config(padx=100, pady=50, bg=YELLOW)
 
 canvas = tk.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
-tomato_img = tk.PhotoImage(file=R"C:\Users\marci\Desktop\Programowanie\KURSY\100_exercise\exercises\days 21-30\day28_tkinter_timer\tomato.png")
+tomato_img = tk.PhotoImage(file="tomato.png")
 canvas.create_image(100,112, image=tomato_img)
 timer_text = canvas.create_text(103,130, text='00:00', fill='white', font=(FONT_NAME, 30, 'bold'))
 canvas.grid(column=1, row=1)
